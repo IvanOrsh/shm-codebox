9a4df44a6f8432a74859ae5f29e0670a35afaa0a
IvanOrsh <ksironov@mail.ru>
2023-12-27 23:19:33 +0300

    feat: select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import type { LanguageName } from "@uiw/codemirror-extensions-langs";
-import { Listbox } from "@headlessui/react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 
 import { GradientBackground } from "@/lib/types";
 import clsx from "clsx";
@@ -40,6 +42,55 @@ export default function Select<T extends GradientBackground | LanguageName>(
           ) : (
             <ThemeBubble color={(initialValue as GradientBackground).value} />
           )}
+
+          <span className="pointer-events-none">
+            <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
+          </span>
+
+          <Transition
+            as={Fragment}
+            enter="transition-all transform ease-in-out duration-200"
+            enterFrom="opacity-0 scale-90"
+            enterTo="opacity-100"
+            leave="transition-all transform ease-in-out duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0 scale-90"
+          >
+            <Listbox.Options
+              className={clsx(
+                "absolute z-10 max-h-80 -translate-x-1/4 -translate-y-3/4 space-y-1 overflow-auto rounded-xl p-2",
+                "border-[1px] border-white/20 bg-black",
+                "focus:outline-none"
+              )}
+            >
+              {options.map((option, i) => (
+                <Listbox.Option
+                  key={`${type}-${i}`}
+                  value={option}
+                  className={clsx(
+                    "flex items-center gap-3 rounded-lg p-2 text-xs",
+                    "cursor-pointer select-none",
+                    "transition-colors duration-200 ease-in-out"
+                  )}
+                >
+                  {type === "language" ? (
+                    <span className="block truncate pr-9">
+                      {option as LanguageName}
+                    </span>
+                  ) : (
+                    <>
+                      <ThemeBubble
+                        color={(option as GradientBackground).value}
+                      />
+                      <span className="block truncate">
+                        {(option as GradientBackground).name}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </Listbox.Button>
       </div>
     </Listbox>
