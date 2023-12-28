d644f947df6c1e5f917815102f4086604f203c47
IvanOrsh <ksironov@mail.ru>
2023-12-28 14:29:46 +0300

    feat: settings component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Code from "@/components/Code";
+import Settings from "@/components/Settings";
 
 export default function Home() {
   return (
-    <main className="h-full flex items-center justify-center flex-col gap-6">
+    <main
+      id="main"
+      className="h-full flex items-center justify-center flex-col gap-6"
+    >
       <h1 className="text-4xl font-black ">
         <span className="text-sky-500 font-extrabold">SHM</span>
         <span className="font-light">codebox</span>
       </h1>
 
       <Code placeholder="Add some code here..." />
+
+      <Settings />
     </main>
   );
 }
