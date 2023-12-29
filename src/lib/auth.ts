5dbd666836d42a9712e9205e7135c128a76f0f88
IvanOrsh <ksironov@mail.ru>
2023-12-29 23:43:58 +0300

    feat: get session helper

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import { Session, getServerSession } from "next-auth";
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export async function getSession(
+  req?: NextApiRequest | GetServerSidePropsContext["req"],
+  res?: NextApiResponse | GetServerSidePropsContext["res"]
+) {
+  if (!req || !res) return (await getServerSession(authOptions)) as Session;
+
+  return (await getServerSession(req, res, authOptions)) as Session;
+}
