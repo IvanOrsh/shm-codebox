726bc8cc72755e962db5fdbbfea8f687ed82642f
IvanOrsh <ksironov@mail.ru>
2023-12-29 13:12:11 +0300

    feat: add supported fonts

---
+++ src/lib/fonts.ts
@@ -0,0 +1,33 @@
+import type { FontDefinition } from "./types";
+
+export const SUPPORTED_FONTS: FontDefinition[] = [
+  {
+    id: "firaCode",
+    label: "Fira Code",
+    value: "var(--font-fira-code)",
+  },
+
+  {
+    id: "jetBrainsMono",
+    label: "JetBrains Mono",
+    value: "var(--font-jetbrains-mono)",
+  },
+
+  {
+    id: "inconsolata",
+    label: "Inconsolata",
+    value: "var(--font-inconsolata)",
+  },
+
+  {
+    id: "sourceCodePro",
+    label: "Source Code Pro",
+    value: "var(--font-source-code-pro)",
+  },
+
+  {
+    id: "ibmPlexMono",
+    label: "IBM Plex Mono",
+    value: "var(--font-ibm-plex-mono)",
+  },
+];
